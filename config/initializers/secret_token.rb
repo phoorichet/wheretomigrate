29d110d851d9addd0f6f3e60a2336bcd85382627
Phoorichet Thepdusith <phoorichet.t@gmail.com>
2013-11-02 11:04:33 -0700

    init the project for ad hackathon

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Wheretomigrate::Application.config.secret_token = '2f14effbfab8c24c5d284d5c80b7c0e3828dc6056a862d57ee107a35ca3f234a1c2f401fdd301ac7ad4b865f2652de2c901b346aa43d787e69f363a2c7305a96'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Wheretomigrate::Application.config.session_store :cookie_store, key: '_wheretomigrate_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Wheretomigrate::Application.config.session_store :active_record_store
