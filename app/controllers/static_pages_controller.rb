517604fb69596833dfc8691294f9d968c2022f28
Manuel Nakamurakare <manhu@Manuels-MacBook-Air.local>
2013-11-09 21:24:04 -0800

    implemting api to get info per city

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,4 +2,22 @@ class StaticPagesController < ApplicationController
   def main
   end
 
+  def city
+
+    city = params[:city]
+
+	cityCostLiving=CostLiving.where("cityname like ?","%#{city}%").first
+	jobs=JobsCity.where("cityname like ?","%#{city}%").first
+	if 	cityCostLiving.nil? || jobs.nil?
+     render json: {}
+    else
+	
+	result={ :costLiving => cityCostLiving.size, :jobs => jobs.numberJobs.to_f / JobsCity.maximum("numberJobs")  }
+	#puts cityCostLiving.compositeIndex
+	render json: result
+	end
+  end
+
+  
+
 end
