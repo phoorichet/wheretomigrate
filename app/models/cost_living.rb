5af162a6e948b67cd8b03cda8eb0ceccb64f5de2
Knott <wittawat@cmu.edu>
2013-11-10 01:57:20 -0800

    Update infowindow display

--- app/models/cost_living.rb
+++ app/models/cost_living.rb
@@ -2,7 +2,14 @@ class CostLiving < ActiveRecord::Base
   attr_accessible :compositeIndex, :groceryItems, :healthcare, :housing, :cityName, :lat, :long, :size, :transportation, :utilities
 
   def html
-   "<b>"+cityName.to_s+"</b><br>Composite Index: "+compositeIndex.to_s+"<br>Grocery Items: "+groceryItems.to_s+"<br>Healthcare Index: "+healthcare.to_s+"<br>Housing Index: "+housing.to_s+"<br>Transportation Index: "+transportation.to_s+"<br>Utilities Index: "+utilities.to_s
+    sprintf("<h2>%s</h2>" + 
+        "<b>Composite Index:</b> <big>%.1f</big><br>" + 
+        "<b>Grocery Items:</b> <big>%.1f</big><br>" + 
+        "<b>Healthcare Index:</b> <big>%.1f</big><br>" + 
+        "<b>Housing Index:</b> <big>%.1f</big><br>" + 
+        "<b>Transportation Index:</b> <big>%.1f</big><br>" + 
+        "<b>Utilities Index:</b> <big>%.1f</big><br>",
+        cityName, compositeIndex.to_f, groceryItems.to_f, healthcare.to_f, housing.to_f, transportation.to_f, utilities.to_f)
   end  		
 
   def as_json options={}
