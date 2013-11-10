816a38fd15dbb64ab25d193e683f00c90dc5c6cf
Manuel Nakamurakare <manhu@Manuels-MacBook-Air.local>
2013-11-09 21:33:20 -0800

    get information for a city

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,11 +8,14 @@ class StaticPagesController < ApplicationController
 
 	cityCostLiving=CostLiving.where("cityname like ?","%#{city}%").first
 	jobs=JobsCity.where("cityname like ?","%#{city}%").first
+	crime=Crime.find_by_county_id(City.where("city like ?","%#{city}%").first.county.id)
+
+
 	if 	cityCostLiving.nil? || jobs.nil?
      render json: {}
     else
 	
-	result={ :costLiving => cityCostLiving.size, :jobs => jobs.numberJobs.to_f / JobsCity.maximum("numberJobs")  }
+	result={ :costLiving => cityCostLiving.size, :jobs => jobs.numberJobs.to_f / JobsCity.maximum("numberJobs"),:crime => crime.size  }
 	#puts cityCostLiving.compositeIndex
 	render json: result
 	end
